00d04ba2d51d42d9a89103eb866e0ce0c1087a40
vip-lic <Administrator@7ADLZNNMD4Q0TEH>
2014-03-17 16:11:42 +0800

    第一次测试git

---
+++ git_java_demo/src/com/org/test/Main.java
@@ -0,0 +1,19 @@
+package com.org.test;
+
+public class Main {
+
+	/** 
+	 * @date   2014-3-17    下午4:04:43 
+	 * @version V1.01   
+	 * @Description: (这里用一句话描述这个方法的作用) 
+	 * @author lichuan
+	 * @param @param args    
+	 * @return void   
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Holl word!!");
+
+	}
+
+}
